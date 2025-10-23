b8501c150ae0434d48a61523838a2e102be117f6
Renaud Wang <renaud.w@outlook.com>
2025-10-23 16:18:54 +0800

    add ShopSample

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import bun from "@nurodev/astro-bun";
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: bun(),
   vite: {
     plugins: [tailwindcss()]
   }
